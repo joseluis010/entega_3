88a1468eb867db6559114742966abc711e031bc7
jose luis <joseluis.aguilar72@gmail.com>
2023-05-24 12:43:36 -0300

    update admin/home

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    título = models.CharField(max_length=100)
+    contenido = models.TextField()
+
+    def __str__(self) -> str:
+        return self.título
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.nombre
+    
